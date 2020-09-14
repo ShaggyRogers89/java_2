a7f9d1866018b05d92301a9b24898bb5d7603197
shaggyrogers <duningrunge89@gmail.com>
2020-09-15 01:38:37 +0300

    HW6. Сделал домашку

--- src/HW6/Client.java
+++ src/HW6/Client.java
@@ -1,46 +1,49 @@
 package HW6;
 
 
-import java.awt.event.ActionEvent;
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 
 public class Client {
-    private Socket socket;
-    DataInputStream in;
-    DataOutputStream out;
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = null;
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+
 
         try {
-            Socket socket = new Socket("localhost", 8188);
+            socket = new Socket("localhost", 8188);
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        while (true) {
-                            String msg = in.readUTF();
-                            if (msg.equals("/end")) {
-                                break;
-                            }
-                            System.out.println("Сервер: " + msg);
-                            out.writeUTF(msg);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        try {
-                            socket.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+            System.out.println("Подключен к серверу");
+
+            Thread threadRead = new Thread(() -> {
+                try {
+                    while (true) {
+                        System.out.println("Скажите что-нибудь серверу: ");
+                        out.writeUTF(scanner.nextLine());
                     }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            }).start();
+            });
+            threadRead.start();
+
+            while (true) {
+                String msg = in.readUTF();
+                if (msg.equals("/end")) {
+                    System.out.println("Сервер отключился");
+                    out.writeUTF("/end");
+                    break;
+                } else {
+                    System.out.println("Сервер пишет:" + msg);
+                }
+            }
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
--- src/HW6/Server.java
+++ src/HW6/Server.java
@@ -5,14 +5,16 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 public class Server {
     private static int PORT = 8188;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ServerSocket server = null;
         Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
 
         try {
             server = new ServerSocket(PORT);
@@ -23,14 +25,28 @@ public class Server {
 
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            Thread threadRead = new Thread(() -> {
+                try {
+                    while (true) {
+                        System.out.println("Скажите что-нибудь клиенту: ");
+                        out.writeUTF(scanner.nextLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            threadRead.start();
+
             while (true) {
                 String msg = in.readUTF();
                 if (msg.equals("/end")) {
                     System.out.println("Клиент отключился");
+                    out.writeUTF("/end");
                     break;
+                } else {
+                    System.out.print("Клиент пишет: " + msg);
                 }
-                System.out.println("Клиент: " + msg);
-                out.writeUTF(msg);
             }
 
         } catch (IOException e) {
